df285a5acf2f0772bd25fd6b1f06764aee938463
skkim <mimcom43@kakao.com>
2025-12-29 23:18:31 +0900

    Fix : 노션 링크 변경

--- src/data/IntroData.jsx
+++ src/data/IntroData.jsx
@@ -2,11 +2,11 @@ export const IntroGroup = [
   {
     id: 1,
     name: "프로젝트 포트폴리오",
-    url: "https://www.notion.so/2841a88239c3800cb75dc6ae92491588?source=copy_link",
+    url: "https://www.notion.so/2d81a88239c3802db7c1e1ca0f3b6494?source=copy_link",
   },
   {
     id: 2,
     name: "경력기술서",
-    url: "https://www.notion.so/2841a88239c380e5aae9c13d614934a7?source=copy_link",
+    url: "https://www.notion.so/2d81a88239c380d598d4eefa98f47c41?source=copy_link",
   },
 ];
