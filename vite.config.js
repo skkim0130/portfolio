7c304c132d0abf576c9664f85867e5e2a62a1d01
skkim <mimcom43@kakao.com>
2025-12-29 22:12:39 +0900

    Refactor : 프로젝트 네임 변경

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/sk-portfolio-react/", // project name
+  base: "/portfolio/", // project name
   plugins: [react()],
   resolve: {
     alias: {
